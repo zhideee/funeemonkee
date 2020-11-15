e530543491ad2f53d3df28de1d7ddcc537389cda
zhideee <59455175+zhideee@users.noreply.github.com>
2020-11-15 20:13:43 +0100

    Add files via upload

---
+++ mobile/index.js
@@ -0,0 +1,5 @@
+function startmusic(){
+    document.getElementById("Audio").play()
+    document.getElementById("play-button").style.opacity = 0;
+    document.getElementById("play-button").style.cursor = "default";
+}
